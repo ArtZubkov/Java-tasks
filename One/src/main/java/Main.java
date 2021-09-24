8c66262f9a50694138e9d7808509a1231f8395cf
ArtZubkov <baqfert@gmail.com>
2021-09-24 12:29:33 +0300

    modified

--- One/src/main/java/Main.java
+++ One/src/main/java/Main.java
@@ -3,30 +3,22 @@ import java.io.IOError;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(calc(1, 1, 6, 3));
-        System.out.println(logicSum(3,8));
+        System.out.println(calc(2, 1, 6, 3));
+        System.out.println(logicSum(3, 8));
     }
 
 
     public static float calc(float a, float b, float c, float d) throws IOError, ArithmeticException, IllegalArgumentException {
         if (d != 0) {
-            float result = a * (b + (c / d));
-            return result;
-        } else {
-            throw new ArithmeticException("Division by zero");
-        }
+            return a * (b + (c / d));
+        } else throw new ArithmeticException("Division by zero");
     }
 
 
     public static boolean logicSum(float a, float b) throws IOError, IllegalArgumentException {
-        boolean result = false;
-        float sum = a+b;
-        if (sum>=10 && sum<=20) {
-            result = true;
-        }
-        return result;
+        float sum = a + b;
+        return sum >= 10 && sum <= 20;
     }
-
 
 
 }
